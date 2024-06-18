b6fcd0779e893c12fabd047f08bf47a2f17d5343
RamyaQE <ramya.rengasamy27@gmail.com>
2024-06-18 15:45:11 +0530

    new project

---
+++ src/com/son/Training.java
@@ -0,0 +1,17 @@
+package com.son;
+
+public class Training {
+
+	private void age() {
+		System.out.println("age is 2");
+	}
+	private void year() {
+		System.out.println("year is 2020");
+	}
+	public static void main(String[] args) {
+		
+		Training t = new Training();
+		t.age();
+		t.year();
+	}
+}
